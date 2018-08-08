4242a6412749554bdad7c1f330f31dd4aae62e70
brian <bphicke@gmail.com>
2018-08-08 10:55:28 -0500

    Make poiler plate server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphQLHTTP = require('express-graphql');
+const path = require('path')
+
+const port = process.env.PORT || 1337;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/graphql', graphQLHTTP({
+    schema,
+    graphql: true
+}));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+//set up index in client folder
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
