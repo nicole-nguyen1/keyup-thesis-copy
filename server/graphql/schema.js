1bbbc5fa928164a48c6ea9cdc6003a831cf8416e
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-11 16:20:27 -0500

    Expand graphql schema and queries for careers

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -8,6 +8,14 @@ const {
   GraphQLID
 } = require('graphql');
 
+const IndustryType = new GraphQLObjectType({
+  name: 'Industry',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString }
+  })
+});
+
 const CareerType = new GraphQLObjectType({
   name: 'Career',
   description: 'This is a career object',
@@ -16,34 +24,101 @@ const CareerType = new GraphQLObjectType({
     id: { type: GraphQLID },
     industry_id: { type: GraphQLID },
     name: { type: GraphQLString },
+    industry: { 
+      type: IndustryType,
+      resolve(parent, args) {
+        return knex('industries')
+                .select()
+                .where({ 'id': parent.industry_id })
+                .first();
+      }
+    },
     annual_salary: { type: GraphQLString },
-    training_length: { type: GraphQLString },
     description: { type: GraphQLString },
     openings: { type: GraphQLString },
-    training_hours: { type: GraphQLString },
-    training_cost: { type: GraphQLString },
     card_image_url: { type: GraphQLString },
     card_pro: { type: GraphQLString},
     profile_image_url: { type: GraphQLString },
     hourly_pay: { type: GraphQLString },
-    video_url: { type: GraphQLString }
+    video_url: { type: GraphQLString },
+    tasks: {
+      type: new GraphQLList(CareerTraitType),
+      resolve(parent, args) {
+        return knex('career_traits')
+          .select()
+          .where({ 'career_id': parent.id, 'type': 'task' });
+      }
+    },
+    skills: {
+      type: new GraphQLList(CareerTraitType),
+      resolve(parent, args) {
+        return knex('career_traits')
+          .select()
+          .where({ 'career_id': parent.id, 'type': 'skill' });
+      }
+    },
+    pros: {
+      type: new GraphQLList(CareerTraitType),
+      resolve(parent, args) {
+        return knex('career_traits')
+          .select()
+          .where({ 'career_id': parent.id, 'type': 'pro' });
+      }
+    },
+    cons: { 
+      type: new GraphQLList(CareerTraitType),
+      resolve(parent, args) {
+        return knex('career_traits')
+                .select()
+                .where({ 'career_id': parent.id, 'type': 'con' });
+      } 
+    },
+    number_of_services: {
+      type: GraphQLInt,
+      resolve(parent, args) {
+        return knex('services')
+          .select()
+          .where({ 'career_id': parent.id })
+          .then((arr) => arr.length);
+      }
+    },
+    training_length: { type: GraphQLString },
+    training_hours: { type: GraphQLString },
+    training_cost: { type: GraphQLString }
+  })
+});
+
+const CareerTraitType = new GraphQLObjectType({
+  name: 'CareerTrait',
+
+  fields: () => ({
+    id: { type: GraphQLID },
+    career_id: { type: GraphQLID },
+    type: { type: GraphQLString },
+    description: { type: GraphQLString }
   })
 });
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    //GET careers list
     careers: {
       type: new GraphQLList(CareerType),
       resolve(parent, args) {
         return knex('careers').select();
       }
     },
+
+    //GET one specific career with its career traits
     career: {
       type: CareerType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return knex('careers').select().where('id', args.id).first(); 
+        return knex('careers')
+                .select()
+                .where('careers.id', args.id)
+                .first(); 
       }
     }
   }
