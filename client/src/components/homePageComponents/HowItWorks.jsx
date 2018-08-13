a4d90e5c9716fdedb1f753d3df4ef94d23cff3ee
Mason Hunter <masonhunter12@gmail.com>
2018-08-13 16:33:05 -0500

    Begin HowItWorks component

---
+++ client/src/components/homePageComponents/HowItWorks.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Typeography from '@material-ui/core/Typography';
+
+const HowItWorks = props => {
+  return (
+    <div>
+      <Typeography variant='subheading'>How KeyUp Works</Typeography>
+      <img/>
+      <Typeography variant='body1'>Learn</Typeography>
+      <Typeography variant='body1'>Get recommendations or browse careers that are in-demand in Austin</Typeography>
+      <img/>
+      <Typeography variant='body1'>Zero In</Typeography>
+      <Typeography variant='body1'>Find trainging services in Austin that fit into your life</Typeography>
+      <img/>
+      <Typeography variant='body1'>Connect</Typeography>
+      <Typeography variant='body1'>Get matched to support programs that can pay for tuition, childcare, etc.</Typeography>
+      <img/>
+      <Typeography variant='body1'>Go!</Typeography>
+      <Typeography variant='body1'>Apply and start on your path to a better future</Typeography>
+    </div>
+  );
+}
+
+export default HowItWorks;
