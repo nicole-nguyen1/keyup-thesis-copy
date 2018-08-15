0d110d77d5e4c3029dac5bb0a81bef478ec8657c
Elyse Vest <evest90@gmail.com>
2018-08-15 11:39:55 -0500

    Make buttons on career list page the proper color

--- client/src/components/Career.jsx
+++ client/src/components/Career.jsx
@@ -48,7 +48,7 @@ class Career extends React.Component {
             </Grid>
             <Grid container spacing={8}>
               <Grid item xs={6}>
-                <Button variant="contained" color="primary" component={Link} to={`/careers/${this.props.career.id}`}>
+                <Button variant="contained" color="primary" style={{backgroundColor: '2979ff'}} component={Link} to={`/careers/${this.props.career.id}`}>
                   LEARN MORE
                 </Button>
               </Grid>
