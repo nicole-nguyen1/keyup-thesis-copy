71b2d82f8141900840148a268f75c9a041d4d1fb
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-16 16:41:55 -0500

    Add ability to share page url via text/sms

--- client/src/components/socialShareComponents/SocialShare.jsx
+++ client/src/components/socialShareComponents/SocialShare.jsx
@@ -5,20 +5,34 @@ import Reddit from './Reddit.jsx';
 import Email from './Email.jsx';
 import Text from './Text.jsx';
 import CopyLink from './CopyLink.jsx';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Drawer from '@material-ui/core/Drawer';
-import Grid from '@material-ui/core/Grid';
 import Snackbar from '@material-ui/core/Snackbar';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { isMobileOnly } from 'react-device-detect';
+import smsLink from 'sms-link';
 
 class SocialShare extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       url: window.location.href,
-      copied: false,
-      snackBarOpen: false
+      snackBarOpen: false,
+      textOpen: false,
+      textLink: ''
     }
+  }
+
+  handleChange = (e) => {
+    this.setState({ 
+      textLink: smsLink({ phone: `1${e.target.value}`, body: this.state.url })
+    });
   }
 
   handleClick = (state) => {
@@ -56,16 +70,69 @@ class SocialShare extends React.Component {
           style={{ textAlign: 'center' }}
         >
           <div style={{ display: 'inline-flex', margin: '20px 5px' }}>
-            <Facebook styles={styles} />
-            <Twitter styles={styles} />
-            <Reddit styles={styles} />
+            <Facebook 
+              styles={styles} 
+              url={this.state.url} />
+            <Twitter 
+              styles={styles} 
+              url={this.state.url} />
+            <Reddit 
+              styles={styles} 
+              url={this.state.url} />
           </div>
           <div style={{ display: 'inline-flex', margin: '20px 5px' }}>
-            <Email styles={styles} />
-            <Text styles={styles} />
-            <CopyLink styles={styles} handleClick={() => this.handleClick('snackBarOpen')}/>
+            <Email 
+              styles={styles} 
+              url={this.state.url} />
+            {/* Show Text component for mobile devices only, excludes tablets */}
+            {isMobileOnly ? 
+              <Text 
+                styles={styles} 
+                url={this.state.url} 
+                handleClick={() => this.handleClick('textOpen')}/> : null}
+            <CopyLink 
+              styles={styles} 
+              url={this.state.url} 
+              handleClick={() => this.handleClick('snackBarOpen')}/>
           </div>
         </Drawer>
+
+        {/* This dialog opens when a user wishes to text the page url to someone */}
+        <Dialog
+          open={this.state.textOpen}
+          onClose={() => this.handleClose('textOpen')}
+          aria-labelledby="text-title"
+        >
+          <DialogTitle id="text-title">{"Share This Page"}</DialogTitle>
+          <DialogContent>
+            <Typography gutterBottom>
+              Enter the phone number of the person you want to text. 
+            </Typography>
+            <Typography>
+              Not sure what their number is? 
+              Click the button below and you'll be able to search your contact list.
+            </Typography>
+            <TextField
+              id="text-recipient-input"
+              fullWidth
+              label="Enter a phone number"
+              value={this.state.phoneNum}
+              onChange={this.handleChange}
+              margin="normal"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.handleClose('textOpen')} color="primary">
+              Cancel
+            </Button>
+            <a href={this.state.textLink} style={{ textDecoration: 'none' }}>
+              <Button color="primary" autoFocus style={{ backgroundColor: '2979ff', color: 'EDEDEE' }}>
+                Send Text
+              </Button>
+            </a>
+          </DialogActions>
+        </Dialog>
+
         {/* The snackbar notification below is a success message once you click 'Copy Link' */}
         <Snackbar
           anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
