79205913c280feda0ea5f6b6956842e1d17493d6
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-16 02:02:55 -0500

    Add ability to send email via contact form on home page

--- client/src/components/homePageComponents/FormHomePage.jsx
+++ client/src/components/homePageComponents/FormHomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -37,54 +38,108 @@ const styles = theme => ({
 class FormHomePage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: '',
+      emailOrPhone: '',
+      email: '',
+      phone: '',
+      message: '',
+      messageSent: false
+    }
+
+    this.submitForm = this.submitForm.bind(this);
   }
 
-  submitForm = (e) => {
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
 
+    if (e.target.name === 'emailOrPhone') {
+      if ((this.state.emailOrPhone).includes('@')) {
+        this.setState({ 
+          email: e.target.value,
+          phone: ''
+        });
+      } else {
+        this.setState({ 
+          phone: e.target.value,
+          email: ''
+        });
+      }
+    }
+  }
+
+  async submitForm(e) {
+    e.preventDefault();
+    const { name, email, phone, message } = this.state;
+    const form = await axios.post('/api/form', { name, email, phone, message });
+    if (form.status === 200) {
+      this.setState({ messageSent: true });
+    }
   }
 
   render() {
     const { classes } = this.props;
+    
     return (
       <Card className={classes.cardStyle}>
         <CardContent className={classes.cardContentStyle}>
-          <Typography variant="headline"className={classes.textStyle} gutterBottom>
-            Chat with one of our
+          {!this.state.messageSent ? 
+            <div>
+              <Typography variant="headline" className={classes.textStyle} gutterBottom>
+                Chat with one of our
             <br />
-            KeyUp Guides Today
+                KeyUp Guides Today
           </Typography>
-          <Typography className={classes.textStyle} gutterBottom>
-            We'll get back to you within 24 hours
+              <Typography className={classes.textStyle} gutterBottom>
+                We'll get back to you within 24 hours
           </Typography>
-          <form onSubmit={this.submitForm} style={{ marginBottom: 0 }}>
-            <FormControl style={{ width: '98%' }}>
-              <Input
-                type="text"
-                placeholder="Your name?"
-                disableUnderline={true}
-                className={classes.inputStyle}
-              />
-              <Input
-                type="text"
-                placeholder="Email Address or Phone Number?"
-                disableUnderline={true}
-                className={classes.inputStyle}
-              />
-              <Input
-                type="text"
-                placeholder="Ask a question or tell us a little about your career interests and priorities..."
-                disableUnderline={true}
-                multiline
-                rows="4"
-                className={classes.inputStyle}
-              />
-            </FormControl>
-            <div style={{ textAlign: 'center' }}>
-              <Button type='submit' variant="contained" color="primary" className={classes.buttonStyle}>
-                GET STARTED
+              <form onSubmit={this.submitForm} style={{ marginBottom: 0 }}>
+                <FormControl style={{ width: '98%' }}>
+                  <Input
+                    type="text"
+                    name="name"
+                    placeholder="Your name?"
+                    disableUnderline={true}
+                    className={classes.inputStyle}
+                    onChange={this.handleChange}
+                  />
+                  <Input
+                    type="text"
+                    name="emailOrPhone"
+                    placeholder="Email Address or Phone Number?"
+                    disableUnderline={true}
+                    className={classes.inputStyle}
+                    onChange={this.handleChange}
+                  />
+                  <Input
+                    type="text"
+                    name="message"
+                    placeholder="Ask a question or tell us a little about your career interests and priorities..."
+                    disableUnderline={true}
+                    multiline
+                    rows="4"
+                    className={classes.inputStyle}
+                    onChange={this.handleChange}
+                  />
+                </FormControl>
+                <div style={{ textAlign: 'center' }}>
+                  <Button type='submit' variant="contained" color="primary" className={classes.buttonStyle}>
+                    GET STARTED
               </Button>
+                </div>
+              </form>
+            </div> : 
+            <div>
+              <Typography variant="headline" className={classes.textStyle} gutterBottom>
+                Your message was successfully sent.
+          </Typography>
+              <Typography className={classes.textStyle} gutterBottom>
+                We'll get back to you within 24 hours!
+          </Typography>
             </div>
-          </form>
+          }
         </CardContent>
       </Card>
     );
--- server/server.js
+++ server/server.js
@@ -9,14 +9,22 @@ const port = process.env.PORT || 1337;
 
 const app = express();
 
+//ROUTES
+const form = require('./routes/form');
+
 app.use(bodyParser.json());
+
+//graphql endpoint
 app.use('/graphql', graphQLHTTP({
   schema,
   graphiql: true
 }));
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use('/api/form', form);
+
 //set up index in client folder
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
 })
