fc49008437c9d52b333189c7f0013d7e9b992420
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-16 19:48:14 -0500

    Allow users to share page URLs by email

--- client/src/components/socialShareComponents/Email.jsx
+++ client/src/components/socialShareComponents/Email.jsx
@@ -21,13 +21,17 @@ class Email extends React.Component {
   };
 
   render() {
+    const url = `mailto:?Subject=KeyUp&Body=Hey%2C%0A%0AI%27ve%20been%20using%20KeyUp%20to%20learn%20more%20about%20careers%2C%20training%20services%2C%20and%20support%20programs.%0A%0AHere%27s%20an%20interesting%20page%20I%20wanted%20to%20share%20with%20you%3A%20${this.props.url}`;
+
     return (
       <div style={this.props.styles.row}>
-        <Grid item
-          role="button">
-          <img src='https://s3.amazonaws.com/key-up-assets/Email-Icon.png' style={this.props.styles.bottomIcon} />
-          <Typography variant='caption'>Email</Typography>
-        </Grid>
+        <a href={url} style={{ textDecoration: 'none' }}>
+          <Grid item
+            role="button">
+            <img src='https://s3.amazonaws.com/key-up-assets/Email-Icon.png' style={this.props.styles.bottomIcon} />
+            <Typography variant='caption'>Email</Typography>
+          </Grid>
+        </a>
       </div>
     )
   }
