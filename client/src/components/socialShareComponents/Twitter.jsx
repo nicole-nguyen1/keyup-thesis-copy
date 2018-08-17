00f006d0c49481409c94e6cd97c68d2b720651b8
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-17 17:26:50 -0500

    Add ability to share page url via reddit

--- client/src/components/socialShareComponents/Twitter.jsx
+++ client/src/components/socialShareComponents/Twitter.jsx
@@ -2,41 +2,22 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-class Twitter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: window.location.href,
-      copied: false,
-      snackBarOpen: false
-    }
-  }
+const Twitter = (props) => {
+  const url = encodeURIComponent(props.url);
+  const text = encodeURIComponent(`I'm using KeyUp to explore careers, training services, and support services! Check out this link: `)
+  const twitterUrl = `https://twitter.com/intent/tweet?url=${url}&text=${text}`;
 
-  handleClick = () => {
-    this.setState({ snackBarOpen: true });
-  };
-
-  handleClose = () => {
-    this.setState({ snackBarOpen: false });
-  };
-
-  render() {
-    const url = encodeURIComponent(this.props.url);
-    const text = encodeURIComponent(`I'm using KeyUp to explore careers, training services, and support services! Check out this link: `)
-    const twitterUrl = `https://twitter.com/intent/tweet?url=${url}&text=${text}`;
-
-    return (
-      <div style={this.props.styles.row}>
-        <a href={twitterUrl} style={{ textDecoration: 'none' }}>
-          <Grid item
-            role="button">
-            <img src='https://s3.amazonaws.com/key-up-assets/Twitter-Logo-True.png' style={this.props.styles.topIcon} />
-            <Typography variant='caption'>Twitter</Typography>
-          </Grid>
-        </a>
-      </div>
-    )
-  }
+  return (
+    <div style={props.styles.row}>
+      <a href={twitterUrl} style={{ textDecoration: 'none' }}>
+        <Grid item
+          role="button">
+          <img src='https://s3.amazonaws.com/key-up-assets/Twitter-Logo-True.png' style={props.styles.topIcon} />
+          <Typography variant='caption'>Twitter</Typography>
+        </Grid>
+      </a>
+    </div>
+  )
 }
 
 export default Twitter;
