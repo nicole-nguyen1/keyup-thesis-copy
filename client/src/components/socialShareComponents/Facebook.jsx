3093bcba852e73c87fd1a5056f10c0656309a39b
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-17 11:46:19 -0500

    Begin work on FB share dialog

--- client/src/components/socialShareComponents/Facebook.jsx
+++ client/src/components/socialShareComponents/Facebook.jsx
@@ -5,30 +5,60 @@ import Typography from '@material-ui/core/Typography';
 class Facebook extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      url: window.location.href,
-      copied: false,
-      snackBarOpen: false
+  }
+
+  componentDidMount() {
+    //Facebook SDK
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: process.env.FB_APP_ID,
+        cookie: true,  // enable cookies to allow the server to access the session
+        xfbml: true,  // parse social plugins on this page
+        version: 'v2.1' // use version 2.1
+      });
+
+      FB.ui({
+        method: 'share',
+        href: this.props.url,
+      }, (response) => {
+        console.log(response);
+      }).bind(this);
+
+      //load FB SDK
+      (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
     }
   }
 
   handleClick = () => {
-    this.setState({ snackBarOpen: true });
-  };
-
-  handleClose = () => {
-    this.setState({ snackBarOpen: false });
+    window.FB.ui({
+      method: 'share',
+      href: this.props.url
+    }, (response) => {
+      if (response && !response.error_message) {
+        alert('Posting completed');
+      } else {
+        alert('Error while posting');
+      }
+    });
   };
 
   render() {
     return (
-      <div style={this.props.styles.row}>
-        <Grid item
-          role="button">
-          <img src='https://s3.amazonaws.com/key-up-assets/facebook-logo-true.png' style={this.props.styles.topIcon} />
-          <Typography variant='caption'>Facebook</Typography>
-        </Grid>
-      </div>
+        <div style={this.props.styles.row}>
+          <a onClick={this.handleClick}>
+            <Grid item
+              role="button">
+              <img src='https://s3.amazonaws.com/key-up-assets/facebook-logo-true.png' style={this.props.styles.topIcon} />
+              <Typography variant='caption'>Facebook</Typography>
+            </Grid>
+          </a>
+        </div>
     )
   }
 }
