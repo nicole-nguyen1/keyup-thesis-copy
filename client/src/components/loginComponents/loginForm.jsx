e284eb0b5e93bb4d1e5333b58d23c8b64f22934a
Elyse Vest <evest90@gmail.com>
2018-08-22 17:06:07 -0500

    Create bare bones of login form

--- client/src/components/loginComponents/loginForm.jsx
+++ client/src/components/loginComponents/loginForm.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+
+const styles = theme => ({
+  inputStyle: {
+    backgroundColor: 'white',
+    margin: '5px 0px',
+    padding: '10px'
+  }
+});
 
 class LoginForm extends React.Component {
   constructor(props) {
@@ -6,12 +18,38 @@ class LoginForm extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
       <div>
+        <Typography variant="headline">
         This works
+        </Typography>
+        <Typography variant='body1'>
+          Sign in to access your KeyUp account
+        </Typography>
+        <FormControl style={{ width: '98%' }}>
+          <Input
+            type="text"
+            name="email"
+            placeholder="Email address"
+            disableUnderline={true}
+            className={classes.inputStyle}
+            onChange={() => console.log('the email input field is changing!')}
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            disableUnderline={true}
+            className={classes.inputStyle}
+            onChange={() => console.log('the password input field is changing!')}
+          />
+        </FormControl>
       </div>
-    )
+    );
   }
 }
 
-export default LoginForm;
+
+
+export default withStyles(styles)(LoginForm);
