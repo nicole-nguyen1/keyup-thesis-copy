bc8ab22c2fe84738e4f5aeb2998de000ac5cc3d2
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-23 12:20:40 -0500

    Allow career_id for favorites to be null

--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -80,7 +80,7 @@ PRIMARY KEY ("id")
 CREATE TABLE "favorites" (
 "id"  SERIAL ,
 "type" VARCHAR NOT NULL DEFAULT 'NULL' ,
-"career_id" INTEGER NOT NULL ,
+"career_id" INTEGER ,
 "service_id" INTEGER ,
 "user_id" INTEGER NOT NULL ,
 PRIMARY KEY ("id")
