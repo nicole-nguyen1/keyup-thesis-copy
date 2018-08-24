bb785b813d6cdfdc59c6d8cc0fd8cf79701895b1
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-24 10:36:10 -0500

    Add user profile and favorites components

---
+++ client/src/components/favoritesComponents/FavoritesContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class FavoritesContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // componentDidMount() {
+
+  // }
+
+  render() {
+    return <Favorites />
+  }
+}
+
+export default FavoritesContainer;
