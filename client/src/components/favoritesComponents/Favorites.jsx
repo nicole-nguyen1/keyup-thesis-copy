f2a5ab0ab0cbd462411aedc40c4c0c64065da36d
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-24 20:30:50 -0500

    Add conditional rendering of info depending on whether or not user has favorites

--- client/src/components/favoritesComponents/Favorites.jsx
+++ client/src/components/favoritesComponents/Favorites.jsx
@@ -7,11 +7,16 @@ import FavoriteTrainings from './FavoriteTrainings.jsx';
 import { createApolloFetch } from 'apollo-fetch';
 import { getCareerFave, getTrainingFave } from '../graphql/graphql';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 const styles = theme => ({
+  paper: {
+    height: '100%'
+  },
+
   tabs: {
-    backgroundColor: '#3651C5',
-    height: '3.5em'
+    backgroundColor: '#3651C5'
   },
 
   tabsIndicator: {
@@ -28,6 +33,21 @@ const styles = theme => ({
 
   tabSelected: {}
 });
+
+function NoFaves(props) {
+  return (
+    <div style={{ padding: '30px 15px' }}>
+      <Typography variant='body1' paragraph>No favorites yet?</Typography>
+      <Typography variant='body1' paragraph>
+        <Link to='/careers' 
+          style={{
+            color: '#4469FF',
+            textDecoration: 'none'
+          }}>Browser Careers and Training</Link> to pick out some likely-looking {props.type}.
+      </Typography>
+    </div>
+  )
+}
 class Favorites extends React.Component {
   constructor(props) {
     super(props);
@@ -76,10 +96,28 @@ class Favorites extends React.Component {
 
   render() {
     const { classes } = this.props;
-    // div with tabs - background color #3651C5
+    let info;
+
+    //for rendering different information based on whether or not a user has favorites
+    //nested conditional statements need to be done this way for easier readability than 
+    //ternary statements
+    if (this.props.favorites[0].id === '' || this.props.favorites === undefined) {
+      if (this.state.value === 0) {
+        info = <NoFaves type='careers' />
+      } else if (this.state.value === 1) {
+        info = <NoFaves type='training services' />
+      }
+    } else {
+      if (this.state.value === 0) {
+        info = <FavoriteCareers careers={this.state.careerFaves} favorites={this.props.favorites} />
+      } else if (this.state.value === 1) {
+        info = <FavoriteTrainings services={this.state.trainingFaves} favorites={this.props.favorites} />
+      }
+    }
+
     return (
       <div>
-        <Paper>
+        <Paper className={classes.paper}>
           <Tabs
             value={this.state.value}
             onChange={this.handleChange}
@@ -103,8 +141,7 @@ class Favorites extends React.Component {
               }}
             />
           </Tabs>
-          {this.state.value === 0 && <FavoriteCareers careers={this.state.careerFaves} favorites={this.props.favorites}/>}
-          {this.state.value === 1 && <FavoriteTrainings services={this.state.trainingFaves} favorites={this.props.favorites}/>}
+          {info}
         </Paper>
       </div>
     )
