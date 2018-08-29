562d5bdab9cf03a9568988ee7aafc2f0b987d7a4
brian <bphicke@gmail.com>
2018-08-29 11:08:34 -0500

    Remove unused Redirect require from loginContainer.jsx

--- client/src/components/loginComponents/loginContainer.jsx
+++ client/src/components/loginComponents/loginContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import { store } from '../../store/index';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
