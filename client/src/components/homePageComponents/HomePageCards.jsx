8ce2df046d796eee99bc8dfd2d23d2482590be2f
Nicole Nguyen <ninguyen00@gmail.com>
2018-08-29 15:54:20 -0500

    Desktop optimization for home page cards

--- client/src/components/homePageComponents/HomePageCards.jsx
+++ client/src/components/homePageComponents/HomePageCards.jsx
@@ -5,30 +5,76 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import { CardActions } from '@material-ui/core';
+import { CardActions, Button } from '@material-ui/core';
+import MediaQuery from 'react-responsive';
 
 const styles = theme => ({
+  outerDivStyle: {
+    background: '#232E49',
+    padding: '20px 5px',
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex'
+    }
+  },
+  innerDivStyle: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex',
+      margin: '0 auto'
+    }
+  },
   cardStyle: {
     background: 'white',
-    margin: '5px'
+    margin: '5px',
+    [theme.breakpoints.up('sm')]: {
+      maxWidth: '360px'
+    }
   },
   headerStyle: {
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    [theme.breakpoints.up('sm')]: {
+      textAlign: 'center',
+      fontSize: '1.5em',
+      paddingBottom: '16px'
+    }
+  },
+  textStyle: {
+    [theme.breakpoints.up('sm')]: {
+      textAlign: 'center',
+      paddingBottom: '16px'
+    }
   },
   cardContentStyle: {
     display: 'flex', 
-    alignItems: 'center'
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
   },
-  divStyle: {
-    background: '#232E49',
-    padding: '20px 5px'
+  iconDiv: {
+    paddingRight: '16px',
+    [theme.breakpoints.up('sm')]: {
+      textAlign: 'center',
+      padding: '20px'
+    }
   },
   icon: {
     height: '26px',
-    marginRight: '16px'
+    [theme.breakpoints.up('sm')]: {
+      height: '70px'
+    }
   },
   cardActionStyle: {
     padding: '0'
+  },
+  button: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+      borderRadius: '0',
+      backgroundColor: '#4469FF',
+      color: '#FFFFFF',
+      margin: '0 auto'
+    }
   }
 });
 
@@ -40,51 +86,67 @@ class HomePageCards extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.divStyle}>
-        <a style={{ textDecoration: 'none' }} href='https://keyup.typeform.com/to/dlfXQi'>
-          <Card className={classes.cardStyle}>
-            <CardContent>
-              <div className={classes.cardContentStyle}>
-                <img src='https://s3.amazonaws.com/key-up-assets/White-chat-on-blue-circle.png' 
-                  className={classes.icon}
-                />
-                <div style={{ flexGrow: '4' }}>
-                  <Typography variant="body1" className={classes.headerStyle}>
-                    Get Recommendations
+      <div className={classes.outerDivStyle}>
+        <div className={classes.innerDivStyle}>
+          <a style={{ textDecoration: 'none' }} href='https://keyup.typeform.com/to/dlfXQi'>
+            <Card className={classes.cardStyle}>
+              <CardContent>
+                <div className={classes.cardContentStyle}>
+                  <div className={classes.iconDiv}>
+                    <img src='https://s3.amazonaws.com/key-up-assets/White-chat-on-blue-circle.png'
+                      className={classes.icon}
+                    />
+                  </div>
+                  <div style={{ flexGrow: '4' }}>
+                    <Typography variant="body1" className={classes.headerStyle}>
+                      Get Career Recommendations
                   </Typography>
-                  <Typography className={classes.textStyle}>
-                    Answer some quick questions to find careers and training
-                    that fit your life
+                    <Typography className={classes.textStyle}>
+                      Answer some quick questions to find careers and training
+                      that fit your life
                   </Typography>
+                  </div>
+                  <MediaQuery query='(max-device-width: 599px)'>
+                    <KeyboardArrowRightIcon style={{ flexGrow: '1' }} />
+                  </MediaQuery>
+                  <Button variant='contained' className={classes.button}>
+                    Get Rec's
+                </Button>
                 </div>
-                <KeyboardArrowRightIcon style={{ flexGrow: '1' }} />
-              </div>
-            </CardContent>
-            <CardActions className={classes.cardActionStyle}></CardActions>
-          </Card>
-        </a>
-        <Link style={{textDecoration: 'none'}} to="/careers">
-          <Card className={classes.cardStyle}>
-            <CardContent>
-              <div className={classes.cardContentStyle}>
-                <img src='https://s3.amazonaws.com/key-up-assets/white-suitcase-on-blue-circle.png' 
-                  className={classes.icon}
-                />
-                <div style={{flexGrow: '4'}}>
-                  <Typography variant="body1"className={classes.headerStyle}>
-            Browse Careers and Training Services
+              </CardContent>
+              <CardActions className={classes.cardActionStyle}></CardActions>
+            </Card>
+          </a>
+          <Link style={{ textDecoration: 'none' }} to="/careers">
+            <Card className={classes.cardStyle}>
+              <CardContent>
+                <div className={classes.cardContentStyle}>
+                  <div className={classes.iconDiv}>
+                    <img src='https://s3.amazonaws.com/key-up-assets/white-suitcase-on-blue-circle.png'
+                      className={classes.icon}
+                    />
+                  </div>
+                  <div style={{ flexGrow: '4' }}>
+                    <Typography variant="body1" className={classes.headerStyle}>
+                      Browse Careers and Training Services
                   </Typography>
-                  <Typography className={classes.textStyle}>
-            Search in-demand careers by salary, distance, training 
-            time, and more
+                    <Typography className={classes.textStyle}>
+                      Search in-demand careers by salary, distance, training
+                      time, and more
                   </Typography>
+                  </div>
+                  <MediaQuery query='(max-device-width: 599px)'>
+                    <KeyboardArrowRightIcon style={{ flexGrow: '1' }} />
+                  </MediaQuery>
+                  <Button variant='contained' className={classes.button}>
+                    Browse
+                </Button>
                 </div>
-                <KeyboardArrowRightIcon style={{flexGrow: '1'}} />
-              </div>
-            </CardContent>
-            <CardActions className={classes.cardActionStyle}></CardActions>
-          </Card>
-        </Link>
+              </CardContent>
+              <CardActions className={classes.cardActionStyle}></CardActions>
+            </Card>
+          </Link>
+        </div>
       </div>
     );
   }
